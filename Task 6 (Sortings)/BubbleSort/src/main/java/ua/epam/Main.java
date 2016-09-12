a40f4b1bbc489a2bfacee6398ac59c5fe499f56f
Mykyta_Dechev <mykyta_dechev@epam.com>
2016-09-12 14:33:49 +0300

    Added Sorting Task

---
+++ Task 6 (Sortings)/BubbleSort/src/main/java/ua/epam/Main.java
@@ -0,0 +1,17 @@
+package ua.epam;
+
+public class Main {
+public static void sort(int[] data) {
+for (int barrier = data.length - 1; barrier >= 0; barrier--) {
+for (int index = 0; index < barrier; index++) {
+if (data[index] > data[index + 1]) {
+int tmp = data[index];
+data[index] = data[index + 1];
+data[index + 1] = tmp;
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Task 6 (Sortings)/BubbleSort/src/main/java/ua/epam/MainTest.java
@@ -0,0 +1,20 @@
+package ua.epam;
+
+import java.util.Arrays;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mykyta_Dechev on 9/12/2016.
+ */
+public class MainTest {
+   @Test
+    public static void main(String[] args) {
+    int[][] data = {{},{5},{6,8,2,9},{5,3,2,1,0}, {7,12,3,50,98,4,13,2,0,9,71}};
+    for (int[] arr : data) {
+    System.out.print(Arrays.toString(arr) + " переходит в ");
+    Main.sort(arr);
+    System.out.println(Arrays.toString(arr));
+         }
+      }
+   }
